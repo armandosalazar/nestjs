51c73b1de0f1f10e65164c0fdfe378b5d77f2021
Armando Salazar <armandosalazar351@gmail.com>
2024-02-08 23:29:10 -0600

    feat: add morgan for logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan'; // @note: Import morgan for logging
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // @note: Use morgan for logging
+  app.use(morgan('dev'));
   await app.listen(3000);
 }
 bootstrap();
